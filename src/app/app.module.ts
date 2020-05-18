6640b4cbec3c0ef794b3c4028883156b33427771
Emanshi <emanshisharma@gmail.com>
2020-05-18 19:25:37 +0530

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserLoginComponent
+    UserLoginComponent,
+    UserRegisterComponent,
+    NavComponent,
+    FooterComponent,
+    HomeComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
