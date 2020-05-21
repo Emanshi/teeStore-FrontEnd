1a6252c6000f6a9e303de766aa49840c5ee1d8a2
Emanshi <emanshisharma@gmail.com>
2020-05-21 20:02:12 +0530

    user profile in progress

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from './profile.service';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
+import { User } from '../models/users';
+import { AuthenticatorService } from '../auth/authenticator.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +12,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  profileForm:FormGroup
+  loggiedIn:boolean
+  loggedInUser:User
+  viewUser:User
+  errorMessage:string
 
-  constructor() { }
+  constructor(private fb:FormBuilder,private auth: AuthenticatorService,private service: ProfileService, private router: Router,private title:Title) {
+    title.setTitle('Profile')
+   }
 
   ngOnInit(): void {
+    this.loggiedIn=false
+    this.auth.sessionUser.subscribe(
+      (data)=>{
+        this.loggedInUser=data;
+        if(data.userName!=null){
+          this.loggiedIn=true
+        }
+      }
+    )
+
+    if(!this.loggiedIn){
+      this.router.navigate(['/home'])      
+    }
+
+    this.getUser()
+  }
+
+  getUser(){
+    this.service.getUser(this.loggedInUser).subscribe(
+
+    )
   }
 
 }
--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../models/users';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getUser(user:User):Observable<User>{
+    return this.http.get<User>(environment.userApi+"getUser/"+user.userId);
+  }
 }
