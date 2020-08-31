91cba93c93cadce3c43c560104ef5cd065e6292f
stubh505 <kaustubh.sarkar505@gmail.com>
2020-08-31 20:42:58 +0530

    Modified server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/tee-store-front-end'));
+app.use(express.static(__dirname + '/dist/teeStoreFrontEnd'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/tee-store-front-end/index.html'));
+res.sendFile(path.join(__dirname+'/dist/teeStoreFrontEnd/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
